a5a1077815a346dc0efa4ec2dc7a8f49f4859113
Icad <beatrixblaine@gmail.com>
2026-02-26 17:14:37 +0700

    build index.html, base.html, static folder

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
